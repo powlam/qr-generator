e54eae18591daf5dd679ddf0498192de7154e390
Paul Albandoz <palbandoz+github@gmail.com>
2024-10-16 09:52:12 +0200

    Front page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/', function (Request $request) {
+    dd($request->input('linkToConvert'), $request->input('qrColor'), $request->input('qrType'));
     return view('welcome');
 });
