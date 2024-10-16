a3941a99372ae22e3170604c61bc31de34fc4f4a
Paul Albandoz <palbandoz+github@gmail.com>
2024-10-16 17:22:58 +0200

    background image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
+            <img id="background" class="fixed top-0 left-0 w-full h-full opacity-40" src="{{ Vite::asset('resources/images/colorful-geometry-shapes-830.jpg') }}" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <form method="post" action="/" class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     @csrf
