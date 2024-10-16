389685d512b0055809893abe07a87b3f886a29d3
Paul Albandoz <palbandoz+github@gmail.com>
2024-10-16 17:38:33 +0200

    Page reorganized

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,24 +21,12 @@
                     @csrf
                     <header class="grid items-center grid-cols-2 gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <button type="submit" class="text-3xl font-bold hover:text-black/70 dark:hover:text-white/70">QR</button>
+                            <button type="submit" class="px-4 py-2 text-3xl font-bold transition duration-300 rounded-md hover:bg-white/70 dark:hover:bg-black/70 hover:text-black/70 dark:hover:text-white/70">QR</button>
                         </div>
                     </header>
 
                     <main class="mt-6">
-                        <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
-                            <div id="qrContainer" class="flex flex-col justify-center items-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
-                                @if ($qr ?? false)
-                                    <div class="bg-white">{!! $qr !!}</div>
-                                    <a href="{{ $link }}" target="_blank" rel="noopener noreferrer"
-                                        class="w-full text-center truncate cursor-pointer"
-                                        title="{{ $link }}"
-                                    >
-                                        {{ $link }}
-                                    </a>
-                                @endif
-                            </div>
-
+                        <div class="grid gap-6 lg:grid-flow-col lg:grid-rows-3 lg:gap-8">
                             <div class="flex items-center justify-between gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] formField">
                                 <label for="linkToConvert inline-block w-20">Link</label>
                                 <input type="text" name="linkToConvert" id="linkToConvert"
@@ -60,6 +48,18 @@
                                     <option value="dot" @selected(($style ?? null) === 'dot')>Puntos</option>
                                     <option value="round" @selected(($style ?? null) === 'round')>Redondeado</option>
                                 </select>
+                            </div>
+
+                            <div id="qrContainer" class="flex flex-col justify-center items-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                                @if ($qr ?? false)
+                                    <div class="bg-white">{!! $qr !!}</div>
+                                    <a href="{{ $link }}" target="_blank" rel="noopener noreferrer"
+                                        class="w-full text-center truncate cursor-pointer"
+                                        title="{{ $link }}"
+                                    >
+                                        {{ $link }}
+                                    </a>
+                                @endif
                             </div>
                         </div>
                     </main>
