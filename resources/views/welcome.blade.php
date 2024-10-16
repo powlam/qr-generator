87346ab0b3f5f7ac97e378460a9cf50be470c894
Paul Albandoz <palbandoz+github@gmail.com>
2024-10-16 11:07:42 +0200

    Generate QR code
    Using Simple QrCode

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,24 +27,38 @@
 
                     <main class="mt-6">
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
-                            <div id="qrContainer" class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                            <div id="qrContainer" class="flex flex-col justify-center items-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                                @if ($qr ?? false)
+                                    <div class="bg-white">{!! $qr !!}</div>
+                                    <a href="{{ $link }}" target="_blank" rel="noopener noreferrer"
+                                        class="w-full text-center truncate cursor-pointer"
+                                        title="{{ $link }}"
+                                    >
+                                        {{ $link }}
+                                    </a>
+                                @endif
                             </div>
 
-                            <div class="flex items-start justify-between gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] formField">
+                            <div class="flex items-center justify-between gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] formField">
                                 <label for="linkToConvert inline-block w-20">Link</label>
-                                <input type="text" name="linkToConvert" id="linkToConvert" class="w-3/4 px-2 py-1 bg-white rounded-md text-black/70" autofocus />
+                                <input type="text" name="linkToConvert" id="linkToConvert"
+                                    value="{{ $link ?? null }}"
+                                    class="w-3/4 px-2 py-1 bg-white rounded-md text-black/70" autofocus />
                             </div>
 
-                            <div class="flex items-start justify-between gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] formField">
+                            <div class="flex items-center justify-between gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] formField">
                                 <label for="qrColor">Color</label>
-                                <input type="color" name="qrColor" id="qrColor" class="w-3/4 px-1 bg-white rounded-md text-black/70" />
+                                <input type="color" name="qrColor" id="qrColor"
+                                    value="{{ $color ?? null }}"
+                                    class="w-3/4 px-1 bg-white rounded-md text-black/70" />
                             </div>
 
-                            <div class="flex items-start justify-between gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] formField">
+                            <div class="flex items-center justify-between gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] formField">
                                 <label for="qrType">Style</label>
                                 <select name="qrType" id="qrType" class="w-3/4 px-2 py-1 bg-white rounded-md text-black/70">
-                                    <option value="1">Cuadraditos</option>
-                                    <option value="2">Puntos</option>
+                                    <option value="square" @selected(($style ?? null) === 'square')>Cuadrados</option>
+                                    <option value="dot" @selected(($style ?? null) === 'dot')>Puntos</option>
+                                    <option value="round" @selected(($style ?? null) === 'round')>Redondeado</option>
                                 </select>
                             </div>
                         </div>
