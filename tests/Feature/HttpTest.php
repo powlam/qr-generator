c2f3a049c3453488614c89ac190b13275d418bc8
Paul Albandoz <palbandoz+github@gmail.com>
2025-03-15 11:17:57 +0100

    chore: composer update

--- tests/Feature/HttpTest.php
+++ tests/Feature/HttpTest.php
@@ -2,15 +2,13 @@
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\Config;
-
 it('returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
 });
 
-it('generates a QR from the web', function() {
+it('generates a QR from the web', function () {
     $response = $this->post('/', ['linkToConvert' => 'http://google.com', 'qrColor' => '#ff2288', 'qrType' => 'square']);
 
     $response
